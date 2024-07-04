5a8b441783e1d2d31a3cde09acc7e6cb715752d1
mad-svg <hkumaran664@gmail.com>
2024-07-04 13:00:39 +0530

    Update ch3.pgm_01.js

---
+++ 03_JS_Objects/ch3.pgm_01.js
@@ -0,0 +1,29 @@
+var bookTitle;
+var bookAuthor;
+
+
+bookTitle = "The Hobbit";
+bookAuthor = "J. R. R. Tolkien";
+
+
+console.log(bookTitle + " by " + bookAuthor);
+
+
+var secondBookTitle = "Harry Potter and the Philosopher's Stone";
+var secondBookAuthor = "J.K. Rowling";
+
+console.log(secondBookTitle + " by " + secondBookAuthor);
+var thirdBookTitle = "1984";
+var thirdBookAuthor = "George Orwell";
+
+
+console.log(thirdBookTitle + " by " + thirdBookAuthor);
+
+Example for 10 books:
+var book1Title = "...";
+var book1Author = "...";
+var book2Title = "...";
+var book2Author = "...";
+...
+var book10Title = "...";
+var book10Author = "...";
